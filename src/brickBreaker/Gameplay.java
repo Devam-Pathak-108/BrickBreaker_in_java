08e067962cff483ab2b151d5b1ea7b64c6ce6e54
Faizan <android1@zlatotech.com>
2024-06-20 08:03:26 +0530

    Initial commit

---
+++ src/brickBreaker/Gameplay.java
@@ -0,0 +1,96 @@
+package brickBreaker;
+
+import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 2;
+    private Timer time;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposY = 120;
+    private int ballDirX = -1;
+    private int ballDirY = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        // time = new Timer(delay,this);
+        // time.start()
+    }
+
+    public void paint(Graphics g) {
+        // background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+        // border
+        g.setColor(Color.blue);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        // the paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // the Ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballposX, ballposY, 20, 20);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX > 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
+    }
+
+}
