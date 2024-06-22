36bdb4ad44a6c27895b696d860e9ee665d261a53
Devam-Pathak-108 <devam2424@gmail.com>
2024-06-22 20:08:34 +0530

    changes controlls a bit

--- src/brickBreaker/Gameplay.java
+++ src/brickBreaker/Gameplay.java
@@ -1,6 +1,5 @@
 package brickBreaker;
 
-import java.awt.event.KeyListener;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -9,7 +8,7 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -41,7 +40,6 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     }
 
     public void paint(Graphics g) {
-        System.out.println("+++++++++++++++++++++++++++++++++");
         // background
         g.setColor(Color.black);
         g.fillRect(1, 1, 692, 592);
@@ -78,6 +76,13 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.yellow);
         g.fillOval(ballposX, ballposY, 20, 20);
 
+        if(totalBricks > 0 && !play && ballposY < 570){
+            
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("Press Enter", 190, 300);
+        }
+
         if (totalBricks <= 0) {
             if (play) {
                 Level++;
@@ -106,7 +111,6 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             g.setFont(new Font("serif", Font.BOLD, 20));
             g.drawString("PRESS ENTER TO RESTART", 230, 350);
         }
-        System.out.println(play+""+totalBricks);
         g.dispose();
     }
 
@@ -118,11 +122,11 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
                 ballDirY = -(1 + Level);
                 ballDirX = -(1 + Level);
             }
-            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 41, 550, 30, 8))) {
+            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 51, 550, 30, 8))) {
                 ballDirY = -(2 + Level);
                 ballDirX = 0;
             }
-            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 41 + 31, 550, 40, 8))) {
+            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX + 51 + 31, 550, 40, 8))) {
                 ballDirY = -(1 + Level);
                 ballDirX = +(1 + Level);
             }
@@ -159,7 +163,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
             if (ballposY < 0) {
                 ballDirY = -ballDirY;
             }
-            if (ballposX > 680) {
+            if (ballposX > 660) {
                 ballDirX = -ballDirX;
             }
         }
@@ -174,18 +178,14 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if (playerX >= 600) {
-                playerX = 600;
-            } else {
+            if (playerX <= 560) {
                 moveRight();
             }
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (playerX < 10) {
-                playerX = 10;
-            } else {
+            if (playerX > 10) {
                 moveLeft();
-            }
+            } 
         }
         if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             if (!play) {
@@ -228,12 +228,12 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     }
 
     public void moveRight() {
-        play = true;
+        // play = true;
         playerX += 15;
     }
 
     public void moveLeft() {
-        play = true;
+        // play = true;
         playerX -= 15;
     }
 
